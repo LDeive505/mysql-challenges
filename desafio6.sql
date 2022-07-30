bd3b3db8733b012efa6487dd58946563e02d30cf
LDeive505 <luisdaviab@gmail.com>
2022-07-30 19:19:17 -0300

    feat: alteração na tabela base + desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,10 @@
 SELECT 
-ROUND(MIN(p.price),2) AS faturamento_minimo,
-ROUND(MAX(p.price),2) AS faturamento_maximo,
-ROUND(AVG(p.price),2) AS faturamento_medio,
-ROUND(SUM(p.price),2) AS faturamento_total
-FROM SpotifyClone.plans AS p
-INNER JOIN SpotifyClone.users AS u
-ON p.plan_id = u.plan_id;
+    ROUND(MIN(p.price), 3) AS faturamento_minimo,
+    MAX(p.price) AS faturamento_maximo,
+    ROUND(AVG(p.price), 2) AS faturamento_medio,
+    ROUND(SUM(p.price), 2) AS faturamento_total
+FROM
+    SpotifyClone.plans AS p
+        INNER JOIN
+    SpotifyClone.users AS u ON p.plan_id = u.plan_id;
+

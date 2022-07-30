6af6d7b6aec41db63e41a8b2bfb8f76390c227df
LDeive505 <luisdaviab@gmail.com>
2022-07-29 21:56:34 -0300

    feat: desafio 1 concluído

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,5 @@
-musics.music_name
+SELECT
+COUNT(DISTINCT m.music_name) AS cancoes,
+COUNT(DISTINCT ar.artist_name) AS artistas,
+COUNT(DISTINCT al.album_name) AS albums
+FROM SpotifyClone.musics AS m, SpotifyClone.artists AS ar, SpotifyClone.albums AS al;
